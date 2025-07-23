13fcd06f193ecd2925860d4ad1aa92a0c6fcc05d
pedro ndong <pndong55@gmail.com>
2025-07-23 13:22:24 +0100

    add models cards

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -7,7 +7,16 @@
 # CarModelInline class
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    
+    search_fields = ['name']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name','description']
+    search_fields = ['name']
 
 # Register models here
+
+admin.site.register(CarMake,CarMakeAdmin)
+admin.site.register(CarModel,CarModelAdmin)
