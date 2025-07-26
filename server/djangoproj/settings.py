f71b8e88d7f1792880ad5da1ef70ea3cc1a26c87
pedro ndong <pndong55@gmail.com>
2025-07-26 10:24:37 +0100

    formating code in pep8 standart with black solve problem line 79 4

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -37,8 +37,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
 ]
-CSRF_TRUSTED_ORIGINS = [
-]
+CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
@@ -105,24 +104,21 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation." \
-        "UserAttributeSimilarityValidator",    
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." \
-        "MinimumLengthValidator",   
-    },
-    
- 
-    {
-        "NAME": "django.contrib.auth.password_validation." \
-        "CommonPasswordValidator",   
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." \
-        "NumericPasswordValidator",   
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
-    
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
+    },
 ]
 
 
